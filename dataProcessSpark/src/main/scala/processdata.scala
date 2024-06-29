267fd60721200426d2574e4130b00725c49e1d13
FlorianBGdu75 <florian.segard-gahery@epita.fr>
2024-06-29 17:38:44 +0200

    filter >0.9 dangerousity linked with topic high-danger-alerts

--- dataProcessSpark/src/main/scala/processdata.scala
+++ dataProcessSpark/src/main/scala/processdata.scala
@@ -36,16 +36,35 @@ object DroneDataProcessor {
     val droneDataStream = rawStream
       .select(from_json($"message", droneDataSchema).as("data"))
       .select("data.*")
+
+    // Filtrage des données pour dangerousity > 0.9
+    val highDangerStream = droneDataStream
+      .filter($"dangerousity" > 0.9)
         
     // Écriture des données structurées dans des fichiers JSON
     val query = droneDataStream.writeStream
       .format("json")
       .option("path", "SaveDatalake") 
-      .option("checkpointLocation", "checkpoint")  
+      .option("checkpointLocation", "checkpoint/all-data")  
       .outputMode("append")
       .trigger(Trigger.ProcessingTime("1 minute"))
       .start()
+    
+    // Écriture des données filtrées (high dangerousity) vers un autre système (ex: Kafka)
+    val highDangerQuery = highDangerStream
+      .selectExpr("CAST(id AS STRING) AS key", "to_json(struct(*)) AS value")
+      .writeStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "localhost:9092")
+      .option("topic", "high-danger-alerts")
+      .option("checkpointLocation", "checkpoint/high-danger")
+      .outputMode("update")
+      .trigger(Trigger.ProcessingTime("1 minute"))
+      .start()
+
+
     query.awaitTermination()
+    highDangerQuery.awaitTermination()
   }
 }
 

ca4f7cd1c13eaea67da50475f654e4573d427a3c
Paul Abi Saad <paul.abi-saad@epita.fr>
2024-07-02 13:16:21 +0200

    feat: send alert via whatsapp

--- consumerSparkAlert/build.sbt
+++ consumerSparkAlert/build.sbt
@@ -9,5 +9,6 @@ libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-sql" % "3.5.1",
   "org.apache.spark" %% "spark-streaming" % "3.5.1",
   "org.apache.spark" %% "spark-streaming-kafka-0-10" % "3.5.1",
-  "org.apache.kafka" % "kafka-clients" % "3.7.0"
+  "org.apache.kafka" % "kafka-clients" % "3.7.0",
+  "com.softwaremill.sttp.client4" %% "core" % "4.0.0-M16",
 )
--- consumerSparkAlert/src/main/scala/consumerAlertProcess.scala
+++ consumerSparkAlert/src/main/scala/consumerAlertProcess.scala
@@ -3,7 +3,18 @@ import org.apache.spark.sql.functions._
 import org.apache.spark.sql.streaming.Trigger
 import org.apache.spark.sql.types._
 
+import sttp.client4.quick._
+import sttp.client4.Response
+import sttp.model.Uri
+
 object ConsumerAlertProcess {
+  def sendMsgWhatsApp(message: String): Unit =
+  {
+    val Uri: Uri = uri"https://api.callmebot.com/whatsapp.php?phone=+33695471584&text=$message&apikey=9366232"
+    val response: Response[String] = quickRequest
+      .get(Uri)
+      .send()
+  }
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder
       .appName("Drone Data Processor")
@@ -39,6 +50,22 @@ object ConsumerAlertProcess {
       .select("data.*")
 
     // TODO Appel de la fonction d'alerte
+    val id = col("data.id")
+    val timestamp = col("data.timestamp")
+    val latitude = col("data.latitude")
+    val longitude = col("data.longitude")
+    val altitude = col("data.altitude")
+    val dangerousity = col("data.dangerousity")
+
+    sendMsgWhatsApp(
+      s"""Alert \n
+      time: ${timestamp.toString()}\n
+      latitude: $latitude\n
+      longitude: $longitude\n
+      altitude: $altitude\n
+      dangerousity: $dangerousity\n
+      """)
+
     val processQuery = droneDataStream
       .selectExpr("CAST(id AS STRING) AS key", "to_json(struct(*)) AS value")
       .writeStream
